e18eda7ab3532668170f70f31f836ecb9cc111db
Gid <Alekseytuchak@gmail.com>
2019-04-30 22:52:41 +0700

    now it moves

--- Project 3004/Assets/Scripts/movement.cs
+++ Project 3004/Assets/Scripts/movement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+
+    Rigidbody2D rb2d;
+    float vertical;
+    float horizontal;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        Vector2 v2 = new Vector2(horizontal, vertical);
+        rb2d.velocity = v2 * speed;
     }
 }
