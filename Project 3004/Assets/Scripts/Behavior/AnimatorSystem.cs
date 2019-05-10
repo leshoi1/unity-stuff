d8b5a92b69917f23c0c8882ddf0223307e99580d
Gid <Alekseytuchak@gmail.com>
2019-05-10 19:16:09 +0700

    branch name says
    -

--- Project 3004/Assets/Scripts/Behavior/AnimatorSystem.cs
+++ Project 3004/Assets/Scripts/Behavior/AnimatorSystem.cs
@@ -7,15 +7,19 @@ public class AnimatorSystem : MonoBehaviour
 
     Animator animator;
     MovementData md;
+    InputData id;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         md = GetComponent<MovementData>();
+        id = GetComponent<InputData>();
+
     }
 
     void Update()
     {
         animator.SetFloat("MovementVec", Mathf.Max(Mathf.Abs(md.xAxis), Mathf.Abs(md.yAxis)));
+        animator.SetBool("Attack", id.skill_1);
     }
 }
---
+++ map generator/Assets/b_MapEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class b_MapEditor : MonoBehaviour 
+{
+    int width,heigth;
+     Texture map;
+    // Start is called before the first frame update
+    void Start()
+    {
+     width=128;
+     heigth=128;
+     var map = new Texture2D(width, heigth, TextureFormat.ARGB32, false);  
+     map.SetPixel(width %2, heigth%2, Color.black);
+     map.SetPixel(1, 1, Color.black);
+
+
+     // Apply all SetPixel calls
+     map.Apply();
+ 
+     // connect texture to material of GameObject this script is attached to
+        var spriteRenderer_ptr=GetComponent<SpriteRenderer>();
+        spriteRenderer_ptr.material.SetTexture("map",map);
+
+        bool[,] Boolmap;
+
+
+    }
+
+  
+
+ 
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
